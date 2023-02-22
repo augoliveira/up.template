9ede630f7ee03304425337dc9f8ece7d79278def
Augusto Developer <aug_oliv@hotmail.com>
2023-02-21 22:05:30 -0300

    jest

--- babel-plugns.js
+++ babel-plugns.js
@@ -1,5 +1,9 @@
 module.exports = {
   'fontawesome-svg-core': {
     license: 'free',
+    presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      '@babel/preset-typescript',
+    ],
   }
 }
